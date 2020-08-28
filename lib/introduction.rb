1ac6ddc8c6f40b21ea706bf64b859978c3b88a82
pleasant-communicator-3558 <sujchand2@gmail.com>
2020-08-28 01:18:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,5 @@ end
  
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is Dan and I am learning to program in Amber"
-  
+  puts "Hi, my name is Dan and I am learning to program in Ember.js."
 end 
